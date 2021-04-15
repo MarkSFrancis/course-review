98ce3724607f4403b7cf386b7439d2739277bca5
MarkSFrancis <mrkfrancis8@gmail.com>
2021-04-15 21:12:10 +0100

    Fixed failing autocomplete test

--- components/DesignSystem/Forms/Autocomplete/AutocompleteInput/AutocompleteInputElement.test.tsx
+++ components/DesignSystem/Forms/Autocomplete/AutocompleteInput/AutocompleteInputElement.test.tsx
@@ -1,9 +1,17 @@
+import { Menu } from "@chakra-ui/menu";
 import { render, screen } from "test-utils";
+import { AutocompleteProvider } from "../AutocompleteContext";
 import { AutocompleteInputElement } from "./AutocompleteInputElement";
 
 describe("AutocompleteInput", () => {
   it("should render the input to the dom", () => {
-    render(<AutocompleteInputElement />);
+    render(
+      <Menu>
+        <AutocompleteProvider value={{ value: undefined, setValue: jest.fn() }}>
+          <AutocompleteInputElement />
+        </AutocompleteProvider>
+      </Menu>
+    );
 
     const input = screen.queryByRole("textbox");
     expect(input).toBeInTheDocument();
