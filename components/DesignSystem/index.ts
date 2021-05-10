c6276ac7f71e45de232f77dfc9c1f6213c6dfd9c
MarkSFrancis <mrkfrancis8@gmail.com>
2021-05-10 09:32:55 +0100

    Fix build

--- components/DesignSystem/index.ts
+++ components/DesignSystem/index.ts
@@ -1,5 +1,7 @@
 export * from "@chakra-ui/react";
 export * from "@chakra-ui/icons";
+export { Icon, createIcon } from "@chakra-ui/react";
+export type { IconProps } from "@chakra-ui/react";
 export { Spinner } from "./Spinner";
 export * from "./Forms";
 export * from "./Layout";
